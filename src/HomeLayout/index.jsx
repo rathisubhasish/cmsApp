a49a0c422a97704d3c42087498e71adc3c63413f
Shruti <shruti.solanki@antrepriz.com>
2026-08-12 18:01:58 +0530

    Client added

--- src/HomeLayout/index.jsx
+++ src/HomeLayout/index.jsx
@@ -8,7 +8,7 @@ export default function HomeLayout() {
     return (
         <div className="flex min-h-svh bg-white overflow-x-hidden">
             <Sidebar setIsMobileOpen={setIsMobileOpen} isMobileOpen={isMobileOpen} />
-            <main className="flex-1 px-6 py-6">
+            <main className="min-w-0 flex-1 px-6 py-6">
                 <Outlet />
             </main>
         </div>
