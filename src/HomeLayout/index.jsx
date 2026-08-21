cb14d0e808b003ade3e65ea6503e4c90b9c0f484
Subhasish <vaibhavkeware@Vaibhavs-Laptop.local>
2026-08-21 16:05:51 +0530

    sidebar fixed as per role and page scroll fix

--- src/HomeLayout/index.jsx
+++ src/HomeLayout/index.jsx
@@ -6,11 +6,15 @@ export default function HomeLayout() {
     const [isMobileOpen, setIsMobileOpen] = useState(false);
 
     return (
-        <div className="flex min-h-svh bg-white overflow-x-hidden">
-            <Sidebar setIsMobileOpen={setIsMobileOpen} isMobileOpen={isMobileOpen} />
-            <main className="min-w-0 flex-1 px-6 py-6">
+        <div className="min-h-svh bg-white overflow-x-hidden">
+            <Sidebar
+                setIsMobileOpen={setIsMobileOpen}
+                isMobileOpen={isMobileOpen}
+            />
+
+            <main className="min-h-[200vh] min-w-0 px-6 py-6 lg:ml-[240px]">
                 <Outlet />
             </main>
         </div>
-    )
+    );
 }
